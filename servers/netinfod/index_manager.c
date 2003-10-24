6b293e7d0eb3d93854f4ae702a53624d0bc24d36
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2003-10-24 23:18:25 +0000

    netinfo-324
    Imported from netinfo-324.tar.gz

--- servers/netinfod/index_manager.c
+++ servers/netinfod/index_manager.c
@@ -3,21 +3,22 @@
  *
  * @APPLE_LICENSE_HEADER_START@
  * 
- * "Portions Copyright (c) 1999 Apple Computer, Inc.  All Rights
- * Reserved.  This file contains Original Code and/or Modifications of
- * Original Code as defined in and that are subject to the Apple Public
- * Source License Version 1.0 (the 'License').  You may not use this file
- * except in compliance with the License.  Please obtain a copy of the
- * License at http://www.apple.com/publicsource and read it before using
- * this file.
+ * Copyright (c) 1999-2003 Apple Computer, Inc.  All Rights Reserved.
+ * 
+ * This file contains Original Code and/or Modifications of Original Code
+ * as defined in and that are subject to the Apple Public Source License
+ * Version 2.0 (the 'License'). You may not use this file except in
+ * compliance with the License. Please obtain a copy of the License at
+ * http://www.opensource.apple.com/apsl/ and read it before using this
+ * file.
  * 
  * The Original Code and all software distributed under the License are
  * distributed on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
  * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
- * License for the specific language governing rights and limitations
- * under the License."
+ * FITNESS FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
+ * Please see the License for the specific language governing rights and
+ * limitations under the License.
  * 
  * @APPLE_LICENSE_HEADER_END@
  */
@@ -85,17 +86,11 @@ im_forget(
 	ni_index i;
 	im_private *priv = (im_private *)handle->private;
 
-	for (i = 0; i < priv->nheld; i++) {
-		if (priv->held[i].refcount == 0) {
-			im_holder_unalloc(&priv->held[i]);
-			priv->held[i] = priv->held[priv->nheld - 1];
-			MM_SHRINK_ARRAY(priv->held, priv->nheld);
-			priv->nheld--;
-			i--;
-		} else {
-			priv->held[i].refcount = 0;
-		}
-	}
+	for (i = 0; i < priv->nheld; i++) im_holder_unalloc(&priv->held[i]);
+
+	free(priv->held);
+	priv->held = NULL;
+	priv->nheld = 0;
 }
 
 void
